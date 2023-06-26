cd4098c57c8fcb822c6549ad4f61ca73bc5f5c15
Jashon Osala <jashonosala@gmail.com>
2023-06-27 02:47:53 +0300

    Fix bugs

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
   const __array = []
   for (const value of array) {
-    var value = array[idx];
     __array.push(appendString + value);
   }
 
