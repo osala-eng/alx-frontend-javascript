269717c4a149febc5707f87e941b178eb019af65
Jashon Osala <jashonosala@gmail.com>
2023-06-29 05:01:58 +0300

    Duplicate promisses

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,13 @@
+export default (mathFunction) => {
+  const queue = [];
+  let mathFunc;
+
+  try {
+    mathFunc = mathFunction();
+  } catch (error) {
+    mathFunc = error.toString();
+  }
+  queue.push(mathFunc);
+  queue.push('Guardrail was processed');
+  return queue;
+};
